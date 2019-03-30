b59036529097d5b2e7bdb1384bcd1f9e97cc1fb6
hsgo1234 <997922078@qq.com>
2019-03-30 23:22:43 +0800

    first commit!!!

---
+++ dormServer/controller/src/main/java/com/qddx/controller/ControllerApplication.java
@@ -0,0 +1,21 @@
+package com.qddx.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class ControllerApplication {
+
+    @RequestMapping("/")
+    String home(){
+        return "ahsjdahkahdka";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
+
+}
